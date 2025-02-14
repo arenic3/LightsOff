ba2b5ada5b2644a7fae5e742870e23b39334b57a
Nicolas Arellano Guzman <narel001@campus.goldmsmiths.ac.uk>
2025-02-14 12:53:18 +0000

    small bug fixes/ early iteration

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function setup() {
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 100 );
     camera.position.set = (10, 10, 10); 
     camera.lookAt(0, 0, 0);
 
@@ -27,8 +27,12 @@ function setup() {
     camera.add( pointLight );
     scene.add( camera );
 
+    const spotLight = new THREE.SpotLight( 0xffffff );
+    spotLight.position.set( 0, -10, 0 );
+    scene.add(spotLight);
+
     const loader = new GLTFLoader();
-    loader.load('assets/untitled.glb', (gltf) => {
+    loader.load('assets/house.glb', (gltf) => {
         mesh = gltf.scene;
         mesh.position.set(0, -4, 4);
         scene.add(mesh);
@@ -40,6 +44,26 @@ function setup() {
 	controls.addEventListener( 'change', animate );
 };
 
+document.addEventListener('keydown', onDocumentKeyDown);
+
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    switch (keyCode) {
+        case 37: // left arrow key
+            mesh.rotation.y += 0.1;
+            break;
+        case 39: // right arrow key
+            mesh.rotation.y -= 0.1;
+            break;
+        case 38: // up arrow key
+            mesh.rotation.x += 0.1;
+            break;
+        case 40: // down arrow key
+            mesh.rotation.x -= 0.1;
+            break;
+    }
+};
+
 function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
@@ -47,9 +71,19 @@ function animate() {
 
 animate();
 
-function gameMechanics() {
+function gameLoop() {
     // Game mechanics here
-}
 
+};
+
+function eventTimer(){
+    //Set intervals for events to go off -> ramp with time
+
+};
+
+function toggleEvent(){
+    //Set off light & sound components
+
+};
 
 
